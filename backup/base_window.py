106055a31e11f165278247d641233396fe15a381
Leo <leonid112@yandex.ru>
2025-11-20 12:01:53 +0300

    final_dash

--- backup/base_window.py
+++ backup/base_window.py
@@ -1,11 +1,13 @@
 from PyQt6.QtWidgets import QWidget, QGridLayout, QLabel, \
     QPushButton, QMessageBox, QListWidget
+from PyQt6.QtGui import QColor
 from db_manager import Db
 
 class BaseWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle('Основное окно')
+        self.setGeometry(10,50,1280,720)
         layout = QGridLayout()
         self.setLayout(layout)
         self.user_role = QLabel('гость')
@@ -25,10 +27,18 @@ class BaseWindow(QWidget):
         sql = 'SELECT * FROM tovar;'
         self.db.cur.execute(sql)
         ans = self.db.cur.fetchone()
+        k=0
         while ans is not None:
             tmp_str=''
             for item in ans:
                 tmp_str += (item +', ')
             self.goods_lst.addItem(tmp_str)
+            if str(ans[7]).isdigit():
+                if int(ans[7]) >15:
+                    self.goods_lst.item(k).setBackground(QColor("red"))
+            if str(ans[8]).isdigit():
+                if int(ans[8]) == 0:
+                    self.goods_lst.item(k).setBackground(QColor("lightGray"))
             ans = self.db.cur.fetchone()
+            k+=1
         
